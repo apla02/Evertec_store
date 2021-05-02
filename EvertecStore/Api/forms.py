6ad6247d90ecd34c28e2949a5e79ab97bc15ab29
apla02 <apla02@gmail.com>
2021-05-02 09:29:14 -0500

    docstring was added

--- EvertecStore/Api/forms.py
+++ EvertecStore/Api/forms.py
@@ -3,5 +3,6 @@ from .models import Orders
 
 class OrderForm(forms.ModelForm):
     class Meta:
+        """detailing model and fiels to be handling by form"""
         model = Orders
         fields = ['customer_name', 'customer_email', 'customer_mobile']
